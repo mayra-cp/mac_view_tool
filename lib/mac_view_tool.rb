601a922631bab9ec0db7095696d3f06a7599d93e
Mayra C <mayra@mayras-mbp.attlocal.net>
2018-12-10 15:49:30 -0800

    Dev tool initial commit

---
+++ lib/mac_view_tool.rb
@@ -0,0 +1,6 @@
+require "mac_view_tool/version"
+require "mac_view_tool/renderer"
+
+module MacViewTool
+  # Your code goes here...
+end
---
+++ lib/mac_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MacViewTool
+  VERSION = "0.1.0"
+end
